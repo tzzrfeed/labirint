343d443b8dd9fd2eaf5a81bd6435bb19765b687f
tzzrfeed <166975436+tzzrfeed@users.noreply.github.com>
2024-04-21 10:38:10 +0500

    Add files via upload

---
+++ labirint.py
@@ -0,0 +1,18 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y):
+        sprite.Sprite.__init__(self)
+        self_image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_x_speed, player_y_speed):
+
+        GameSprite.__init__(self, player_image, player_x, player_y.size_x
+
